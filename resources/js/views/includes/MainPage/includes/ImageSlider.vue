add631c4f2f15851418f5083e625207752a7b144
nagytommy76 <nagytommy76@gmail.com>
2022-05-28 14:40:35 +0200

    Added new cloud images to compStore

--- resources/js/views/includes/MainPage/includes/ImageSlider.vue
+++ resources/js/views/includes/MainPage/includes/ImageSlider.vue
@@ -1,9 +1,12 @@
 <template>
     <div class="image-slider">
-        <span @click="$emit('close')" class="image-close"><font-awesome :icon="['far', 'times-circle']" size="2x"/></span>
+        <span @click="$emit('close')" class="image-close"
+            ><font-awesome :icon="['far', 'times-circle']" size="2x"
+        /></span>
         <transition v-if="!singleImage" name="slide-image">
-            <CloudImage 
-                v-for="nth in [step]" :key="nth"
+            <CloudImage
+                v-for="nth in [step]"
+                :key="nth"
                 @click.native="$emit('close')"
                 :folder="imgFolderName"
                 :image="allImages[nth]"
@@ -11,124 +14,140 @@
             />
         </transition>
         <div @click="$emit('close')" class="img-container" v-if="singleImage">
-            <img :src="`/images/${imgFolderName}.jpg`">
+            <img :src="`/images/${imgFolderName}.jpg`" />
         </div>
         <div v-if="!singleImage" class="arrow-right" @click="increase">
             <Tooltip :text="nextPage">
-                <font-awesome class="svg-icon" :icon="['fas' ,'arrow-right']" size="3x"/>
+                <font-awesome
+                    class="svg-icon"
+                    :icon="['fas', 'arrow-right']"
+                    size="3x"
+                />
             </Tooltip>
         </div>
         <div v-if="!singleImage" class="arrow-left" @click="decrease">
             <Tooltip :text="prevPage">
-                <font-awesome class="svg-icon" :icon="['fas', 'arrow-left']" size="3x"/>
+                <font-awesome
+                    class="svg-icon"
+                    :icon="['fas', 'arrow-left']"
+                    size="3x"
+                />
             </Tooltip>
         </div>
     </div>
 </template>
 <script>
 export default {
-    props:{
+    props: {
         imgFolderName: {
             type: String,
-            required: false,
+            required: false
         },
         singleImage: {
             type: Boolean,
             required: false,
             default: false
-        },
+        }
     },
-    mounted(){
+    mounted() {
         switch (this.imgFolderName) {
-            case 'Wargaming':
-                this.getWGImagesFromFolderByName()
+            case "Wargaming":
+                this.getWGImagesFromFolderByName();
                 break;
-            case 'Meatball':
-                this.getMeatBallImagesFromFolderByName()
+            case "Meatball":
+                this.getMeatBallImagesFromFolderByName();
                 break;
-            case 'React Recipe':
-                this.getReactRecipeImagesFromFolderByName()
+            case "React Recipe":
+                this.getReactRecipeImagesFromFolderByName();
                 break;
-            case 'CompStoreMERN':
-                this.getCompStoreImagesFromFolderByName()
+            case "CompStoreMERN":
+                this.getCompStoreImagesFromFolderByName();
                 break;
         }
     },
     data() {
         return {
             step: 0,
-            nextPage: 'Következő',
-            prevPage: 'Nincs előző kép',
-            allImages: [],
-        }
+            nextPage: "Következő",
+            prevPage: "Nincs előző kép",
+            allImages: []
+        };
     },
     methods: {
         // A require.context() build-elésnél fut le, így nem lehet template literalt használni mert az futási időben "adódik át"
         // Ezért kell 3 vagy több függvény...
-        getMeatBallImagesFromFolderByName(){
+        getMeatBallImagesFromFolderByName() {
             const allMeatballImages = [
-                'meatball-main-page_z2gqjt',
-                'pizza_wp57mc',
-                'meal_lpqmay',
-                'cart_s1dxdy',
-                'paypal_ir1kue',
-                'input-admin_rcjs2y',
-                'modify_f1ybut'
-            ]
-            this.fillImages(allMeatballImages)
+                "meatball-main-page_z2gqjt",
+                "pizza_wp57mc",
+                "meal_lpqmay",
+                "cart_s1dxdy",
+                "paypal_ir1kue",
+                "input-admin_rcjs2y",
+                "modify_f1ybut"
+            ];
+            this.fillImages(allMeatballImages);
         },
-        getWGImagesFromFolderByName(){
-            const allWGImages = ['search_evfww8','modal_moxvgh']
-            this.fillImages(allWGImages)
+        getWGImagesFromFolderByName() {
+            const allWGImages = ["search_evfww8", "modal_moxvgh"];
+            this.fillImages(allWGImages);
         },
-        getReactRecipeImagesFromFolderByName(){
+        getReactRecipeImagesFromFolderByName() {
             const allRecipeImages = [
-                'React_Recipe_Finder_WelcomePage1_lvecwz',
-                'React_Recipe_Finder_fvspcp',
-                'React_Recipe_Finder_videos_qrclha',
-                'React_Recipe_Finder_menuItems_n0zzc9',
-                'React_Recipe_Finder_details_jci5zy'
-            ]
-            this.fillImages(allRecipeImages)
+                "React_Recipe_Finder_WelcomePage1_lvecwz",
+                "React_Recipe_Finder_fvspcp",
+                "React_Recipe_Finder_videos_qrclha",
+                "React_Recipe_Finder_menuItems_n0zzc9",
+                "React_Recipe_Finder_details_jci5zy"
+            ];
+            this.fillImages(allRecipeImages);
         },
-        getCompStoreImagesFromFolderByName(){
+        getCompStoreImagesFromFolderByName() {
             const allCompStoreImages = [
-                'login_qmhzu5',
-                'cpu_yom9xn',
-                'cart_ttz7ga',
-                'admin-vga-modify_wjrbhi',
-                'full-height-cpu_qfz5ay',
-            ]
-            this.fillImages(allCompStoreImages)
+                // "login_qmhzu5",
+                // "cpu_yom9xn",
+                // "cart_ttz7ga",
+                // "admin-vga-modify_wjrbhi",
+                // "full-height-cpu_qfz5ay"
+                "CPUShop_pdvn8x",
+                "HDDShop_eg6zkt",
+                "cart_ttz7ga",
+                "Details_jfyguo",
+                "Admin_uig7lb",
+                "OrderSummary_gx3nwo"
+            ];
+            this.fillImages(allCompStoreImages);
         },
-        fillImages(imagesNameFromFolder){
-            imagesNameFromFolder.forEach(imageName => this.allImages.push(imageName))
+        fillImages(imagesNameFromFolder) {
+            imagesNameFromFolder.forEach(imageName =>
+                this.allImages.push(imageName)
+            );
         },
-        increase(){
-            if(this.allImages[this.step + 1] !== undefined){
-                ++this.step
+        increase() {
+            if (this.allImages[this.step + 1] !== undefined) {
+                ++this.step;
                 if (this.allImages[this.step - 1] !== undefined) {
-                    this.prevPage = 'Előző'
+                    this.prevPage = "Előző";
                 }
             }
-            if(this.allImages[this.step + 1] === undefined){
-                this.nextPage = 'Nincs több kép'
+            if (this.allImages[this.step + 1] === undefined) {
+                this.nextPage = "Nincs több kép";
             }
         },
-        decrease(){
+        decrease() {
             if (this.allImages[this.step - 1] !== undefined) {
-                --this.step
+                --this.step;
                 if (this.allImages[this.step + 1] !== undefined) {
-                    this.nextPage = 'Következő'
+                    this.nextPage = "Következő";
                 }
             }
-            if(this.allImages[this.step - 1] === undefined){
-                this.prevPage = 'Nincs előző kép'
+            if (this.allImages[this.step - 1] === undefined) {
+                this.prevPage = "Nincs előző kép";
             }
-        },
-    },
-}
+        }
+    }
+};
 </script>
 <style lang="scss">
-@import '../../../../../sass/inc/portfolio/image_slider.scss';
+@import "../../../../../sass/inc/portfolio/image_slider.scss";
 </style>
